0649864cabb53527911f327c224b41b99c88afeb
JHelpgg <jhelpgg@gmail.com>
2015-11-21 09:18:41 +0100

    Update

--- src/jhelp/android/api/Utilities.java
+++ src/jhelp/android/api/Utilities.java
@@ -1,5 +1,7 @@
 package jhelp.android.api;
 
+import java.lang.reflect.Array;
+
 /**
  * Some utilities
  * 
@@ -7,6 +9,80 @@ package jhelp.android.api;
  */
 public class Utilities
 {
+   public static final <T> T[] copy(final T[] array)
+   {
+      if(array == null)
+      {
+         return null;
+      }
+
+      return Utilities.copy(array, 0);
+   }
+
+   public static final <T> T[] copy(final T[] array, final int offset)
+   {
+      if(array == null)
+      {
+         return null;
+      }
+
+      return Utilities.copy(array, offset, array.length);
+   }
+
+   public static final <T> T[] copy(final T[] array, int offset, int length)
+   {
+      if(array == null)
+      {
+         return null;
+      }
+
+      if(offset < 0)
+      {
+         length += offset;
+         offset = 0;
+      }
+
+      length = Math.max(0, Math.min(array.length - offset, length));
+
+      if(length <= 0)
+      {
+         return (T[]) Array.newInstance(array.getClass().getComponentType(), 0);
+      }
+
+      final T[] copy = (T[]) Array.newInstance(array.getClass().getComponentType(), length);
+      System.arraycopy(array, offset, copy, 0, length);
+
+      return copy;
+   }
+
+   public static final <T> void scramble(final T[] array)
+   {
+      if(array == null)
+      {
+         return;
+      }
+
+      final int size = array.length;
+      if(size < 2)
+      {
+         return;
+      }
+
+      int i, j;
+      final int time = (size * 2) + (size / 2) + 1;
+      T temp;
+
+      for(int t = 0; t < time; t++)
+      {
+         i = (int) (Math.random() * size);
+         j = (int) (Math.random() * size);
+
+         temp = array[i];
+         array[i] = array[j];
+         array[j] = temp;
+      }
+   }
+
    /**
     * Make caller thread sleep for a time in millisencond
     * 
